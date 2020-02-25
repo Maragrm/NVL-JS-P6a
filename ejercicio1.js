58da44a9c953d78d6d7c07011fee0c72fde90638
Maragrm <60449910+Maragrm@users.noreply.github.com>
2020-02-25 19:47:45 +0100

    Add files via upload

---
+++ ejercicio1.js
@@ -0,0 +1,7 @@
+alert("Hola Mundo con JS")
+var nombre_y_apellidos = "Maravillas García-Ripoll";
+var edad = "27";
+var dias_de_la_semana = "Lunes, Martes, Miércoles, Jueves, Viernes, Sábado, Domingo";
+var valor_de_la_gravedad = "9.8 m/s2";
+var Los_tres_primeros_numeros_impares = "1, 3, 5";
+console.log(nombre_y_apellidos, edad);
